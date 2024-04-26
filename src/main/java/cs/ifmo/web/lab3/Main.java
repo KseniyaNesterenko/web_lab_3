bc9624f8dcae4157d94a2613988d6b1c77b1cd9c
DKhorobrykh <horobryh.daniil@yandex.ru>
2024-04-26 10:28:41 +0300

    curr rev2

--- src/main/java/cs/ifmo/web/lab3/Main.java
+++ src/main/java/cs/ifmo/web/lab3/Main.java
@@ -2,6 +2,6 @@ package cs.ifmo.web.lab3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("It's working. Revision current");
+        System.out.println("It's working. Revision current222");
     }
 }
